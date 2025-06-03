7d52a530b8fc1b6776d27a61c908639ac23614d7
SportsByNumbers <brettclark79@gmail.com>
2025-06-03 14:50:38 +1000

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
+/ babel.config.js
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
+    '@babel/preset-env', // Handles modern JS features like async/await for target environments
+    '@babel/preset-react' // Handles JSX
   ],
 };
